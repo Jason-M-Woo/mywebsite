47e125e78b2d6997ea23e2d7db2c4c9e33fc2d46
Jason Woo <jasonwoo@Jasons-iMac.local>
2018-07-29 14:53:46 -0400

    added register page with post request, no database

--- application.py
+++ application.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contactme.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/success", methods=["POST"])
+def registered():
+    username = request.form.get("username")
+    #password = request.form.get("password")
+    return render_template("registered.html", username=username)
