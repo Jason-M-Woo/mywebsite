861e5737447a861a7b2c98e6fcfeef8bab403d18
Jason Woo <jasonwoo@Jasons-iMac.local>
2018-07-30 19:57:17 -0400

    added new register alerts

--- application.py
+++ application.py
@@ -17,10 +17,6 @@ def index():
 def contact():
     return render_template("contactme.html")
 
-@app.route("/success")
-def successful():
-    return render_template("registered.html", username=request.form.get("username"))
-
 @app.route("/login")
 def login():
     return render_template("login.html")
@@ -30,15 +26,19 @@ def tryRegister():
     nameToCheck = request.form.get("username")
     passToCheck = request.form.get("password")
     passToCheck2 = request.form.get("confirm_password")
-    if checkStringValid(nameToCheck) and checkStringValid(passToCheck) and (passToCheck == passToCheck2):
+    if checkStringValid(nameToCheck) and checkStringValid(passToCheck) and (passToCheck == passToCheck2) and (len(nameToCheck) < 17) and (len(passToCheck) > 7):
         if checkUsernameUnique(nameToCheck):
             updateAccountDB(nameToCheck, passToCheck)
-            return redirect("/success")
+            return render_template("registered.html", nameToCheck)
         else:
             return returnInvalidInfo("Username already taken")
     else:
         if passToCheck != passToCheck2:
             return returnInvalidInfo("Passwords did not match")
+        elif len(nameToCheck) > 16:
+            return returnInvalidInfo("Username must be 16 characters or less")
+        elif len(passToCheck) < 9:
+            return returnInvalidInfo("Password must be 8 characters or more")
         else:
             return returnInvalidInfo("Please enter an alphanumeric username/password")
 
