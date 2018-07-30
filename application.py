0ef7f8f90859aba541f3fdcf3f341bfadcc435a8
Jason Woo <jasonwoo@Jasons-iMac.local>
2018-07-30 19:47:35 -0400

    made not-functioning login page with functional register page

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from applicationdb import checkUsernameUnique, updateAccountDB
 
 def returnInvalidInfo(message):
@@ -9,7 +9,7 @@ def checkStringValid(stringToCheck):
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
     return render_template("index.html")
 
@@ -17,19 +17,23 @@ def index():
 def contact():
     return render_template("contactme.html")
 
-@app.route("/register")
-def register():
-    return render_template("register.html")
+@app.route("/success")
+def successful():
+    return render_template("registered.html", username=request.form.get("username"))
 
-@app.route("/success", methods=["POST"])
-def registered():
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/registerattempt", methods=["POST"])
+def tryRegister():
     nameToCheck = request.form.get("username")
     passToCheck = request.form.get("password")
     passToCheck2 = request.form.get("confirm_password")
     if checkStringValid(nameToCheck) and checkStringValid(passToCheck) and (passToCheck == passToCheck2):
         if checkUsernameUnique(nameToCheck):
             updateAccountDB(nameToCheck, passToCheck)
-            return render_template("registered.html", username=nameToCheck)
+            return redirect("/success")
         else:
             return returnInvalidInfo("Username already taken")
     else:
@@ -37,3 +41,7 @@ def registered():
             return returnInvalidInfo("Passwords did not match")
         else:
             return returnInvalidInfo("Please enter an alphanumeric username/password")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
