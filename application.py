ecde6af3a11d0e16d6d5149cc9bc9b8b07078b79
Jason Woo <jasonwoo@Jasons-iMac.local>
2018-07-31 19:36:21 -0400

    made login page functional

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect
-from applicationdb import checkUsernameUnique, updateAccountDB
+from applicationdb import checkUsernameUnique, updateAccountDB, checkLoginInfo
 
-def returnInvalidInfo(message):
-    return render_template("register.html", usernamealert = message)
+def returnInvalidInfo(webpage, message):
+    return render_template(webpage, usernamealert = message)
 
 def checkStringValid(stringToCheck):
     return stringToCheck.isalnum()
@@ -17,31 +17,44 @@ def index():
 def contact():
     return render_template("contactme.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/registerattempt", methods=["POST"])
-def tryRegister():
-    nameToCheck = request.form.get("username")
-    passToCheck = request.form.get("password")
-    passToCheck2 = request.form.get("confirm_password")
-    if checkStringValid(nameToCheck) and checkStringValid(passToCheck) and (passToCheck == passToCheck2) and (len(nameToCheck) < 17) and (len(passToCheck) > 7):
-        if checkUsernameUnique(nameToCheck):
-            updateAccountDB(nameToCheck, passToCheck)
-            return render_template("registered.html", nameToCheck)
-        else:
-            return returnInvalidInfo("Username already taken")
-    else:
-        if passToCheck != passToCheck2:
-            return returnInvalidInfo("Passwords did not match")
-        elif len(nameToCheck) > 16:
-            return returnInvalidInfo("Username must be 16 characters or less")
-        elif len(passToCheck) < 9:
-            return returnInvalidInfo("Password must be 8 characters or more")
-        else:
-            return returnInvalidInfo("Please enter an alphanumeric username/password")
-
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        nameToCheck = request.form.get("username")
+        passToCheck = request.form.get("password")
+        passToCheck2 = request.form.get("confirm_password")
+        if checkStringValid(nameToCheck) and checkStringValid(passToCheck) and (passToCheck == passToCheck2) and (len(nameToCheck) < 17) and (len(passToCheck) > 7):
+            if checkUsernameUnique(nameToCheck):
+                updateAccountDB(nameToCheck, passToCheck)
+                return render_template("registered.html", username=nameToCheck)
+            else:
+                return returnInvalidInfo("register.html", "Username already taken")
+        else:
+            if passToCheck != passToCheck2:
+                return returnInvalidInfo("register.html", "Passwords did not match")
+            elif len(nameToCheck) > 16:
+                return returnInvalidInfo("register.html", "Username must be 16 characters or less")
+            elif len(passToCheck) < 9:
+                return returnInvalidInfo("register.html", "Password must be 8 characters or more")
+            else:
+                return returnInvalidInfo("register.html", "Please enter an alphanumeric username/password")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    nameToCheck = request.form.get("name")
+    passToCheck = request.form.get("pass")
+    correctInfo = checkLoginInfo(nameToCheck, passToCheck)
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        if checkStringValid(nameToCheck) and checkStringValid(passToCheck):
+            if correctInfo == True:
+                return render_template("loggedin.html", username=nameToCheck)
+            elif correctInfo == "No Account":
+                return render_template("login.html", usernamealert="No existing account for username '{}'".format(nameToCheck))
+            else:
+                return render_template("login.html", usernamealert="Incorrect username/password")
+        else:
+            return returnInvalidInfo("login.html", "Please enter a valid (alphanumeric) username/password")
--- applicationdb.py
+++ applicationdb.py
@@ -13,6 +13,23 @@ def checkUsernameUnique(usernameToCheck):
         return False
     db.close()
 
+def checkLoginInfo(usernameToCheck, passwordToCheck):
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select username from users where username = '{}';".format(usernameToCheck))
+    name = c.fetchone()
+    if name is None:
+        return "No Account"
+    d = db.cursor()
+    d.execute("select password from users where username = '{}';".format(usernameToCheck))
+    password = d.fetchone()
+    if (passwordToCheck == password[0]):
+        return True
+    else:
+        return False
+
+    db.close()
+
 def updateAccountDB(username, password):
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
